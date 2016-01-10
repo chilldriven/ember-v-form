fb4addaa7b9df514ee4a779db8b1e9e626f94424
Anastasia Kucherova <lacataline@gmail.com>
2016-01-10 19:12:41 +0300

    remove bootswatch

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,11 +5,6 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
     var app = new EmberAddon(defaults, {
-        // 'ember-cli-bootswatch': {
-        //     theme: 'paper',
-        //     excludeJS: true,
-        //     excludeFonts: true
-        // }
     });
 
     /*
